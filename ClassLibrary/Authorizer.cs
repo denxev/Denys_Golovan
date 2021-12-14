cb500a09c38f4994e850197a8e5fecb49424ea67
IlliaMamonov <iliamamonov2@gmail.com>
2021-12-14 20:59:11 +0200

    Laba Selenium Completed

---
+++ ClassLibrary/Authorizer.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+
+namespace ClassLibrary
+{
+    public  class Authorizer
+    {
+        private  By _password = By.Name("txtPassword");
+        private  By _submit = By.Name("Submit");
+        private  By _userName = By.Name("txtUsername");
+        private  IWebDriver _driver;
+
+        public Authorizer(IWebDriver webDriver) => _driver = webDriver;
+
+        public void LogIn(string username, string password)
+        {
+            _driver.FindElement(_userName).SendKeys(username);
+            _driver.FindElement(_password).SendKeys(password);
+            _driver.FindElement(_submit).Click();
+        }
+    }
+}
---
+++ ClassLibrary/TitleAdder.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class TitleAdder
+    {
+        private  By _addTitle = By.Name("btnAdd");
+        private  By _titleName = By.Id("jobTitle_jobTitle");
+        private By _description = By.Id("jobTitle_jobDescription"); //Lorem ipsum dolor 
+        private  By _note = By.Id("jobTitle_note"); // sit amet
+        private By _save = By.Id("btnSave");
+        private  IWebDriver _driver;
+
+        public TitleAdder(IWebDriver webDriver) => _driver = webDriver;
+
+        public IWebDriver Driver => _driver;
+
+        public void AddNewRecord(string name)
+        {
+            _driver.FindElement(_addTitle).Click();
+            _driver.FindElement(_titleName).SendKeys(name);
+            _driver.FindElement(_description).SendKeys("Lorem ipsum dolor");
+            _driver.FindElement(_note).SendKeys("sit amet");
+            _driver.FindElement(_save).Click();
+        }
+    }
+}
